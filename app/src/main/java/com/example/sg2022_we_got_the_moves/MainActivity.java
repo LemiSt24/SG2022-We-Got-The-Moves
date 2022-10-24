67e49300663db87ff4732cdff4040d08791b9bf4
LemiSt24 <lennard.strohmeyer@gmail.com>
2022-10-24 12:05:02 +0200

    leeres Projekt

---
+++ app/src/main/java/com/example/sg2022_we_got_the_moves/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sg2022_we_got_the_moves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
